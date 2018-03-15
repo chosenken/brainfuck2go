ddc44825538b551153bfa4b535e62d6a781e6b7a
Ken Herner <kherner@navistone.com>
2018-03-15 15:13:05 -0400

    Initial work on for loop optimizations
    Signed-off-by: Ken Herner <kherner@navistone.com>

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
 
 const (
@@ -22,6 +22,7 @@ const (
 	READIN
 	BEGINLOOP
 	ENDLOOP
+	SETZERO
 )
 
 type OptCode struct {
@@ -49,27 +50,25 @@ func main() {
 	}
 	program := string(buf)
 	programLen := len(program)
-	optCodes := make([]*OptCode, 0)
+	optCodes := NewFifo()
 	last := &OptCode{
 		Type: -1,
 	}
 	hasReadin := false
 	hasOutput := false
 	for i := 0; i < programLen; i++ {
-		if len(optCodes) > 0 {
-			last = optCodes[len(optCodes)-1]
-			optCodes = optCodes[:len(optCodes)-1]
+		if optCodes.HasNext() {
+			last = optCodes.PopLast()
 		}
 		switch program[i] {
 		case '>':
 			if last.Type == PTUP {
 				last.Value++
-				optCodes = append(optCodes, last)
 			} else {
 				if last.Type != -1 {
-					optCodes = append(optCodes, last)
+					optCodes.Push(last)
 				}
-				optCodes = append(optCodes, &OptCode{
+				optCodes.Push(&OptCode{
 					Type:  PTUP,
 					Value: 1,
 				})
@@ -77,12 +76,11 @@ func main() {
 		case '<':
 			if last.Type == PTDOWN {
 				last.Value++
-				optCodes = append(optCodes, last)
 			} else {
 				if last.Type != -1 {
-					optCodes = append(optCodes, last)
+					optCodes.Push(last)
 				}
-				optCodes = append(optCodes, &OptCode{
+				optCodes.Push(&OptCode{
 					Type:  PTDOWN,
 					Value: 1,
 				})
@@ -90,12 +88,11 @@ func main() {
 		case '+':
 			if last.Type == VALUEUP {
 				last.Value++
-				optCodes = append(optCodes, last)
 			} else {
 				if last.Type != -1 {
-					optCodes = append(optCodes, last)
+					optCodes.Push(last)
 				}
-				optCodes = append(optCodes, &OptCode{
+				optCodes.Push(&OptCode{
 					Type:  VALUEUP,
 					Value: 1,
 				})
@@ -103,54 +100,55 @@ func main() {
 		case '-':
 			if last.Type == VALUEDOWN {
 				last.Value++
-				optCodes = append(optCodes, last)
 			} else {
 				if last.Type != -1 {
-					optCodes = append(optCodes, last)
+					optCodes.Push(last)
 				}
-				optCodes = append(optCodes, &OptCode{
+				optCodes.Push(&OptCode{
 					Type:  VALUEDOWN,
 					Value: 1,
 				})
 			}
 		case '.':
 			if last.Type != -1 {
-				optCodes = append(optCodes, last)
+				optCodes.Push(last)
 			}
-			optCodes = append(optCodes, &OptCode{
+			optCodes.Push(&OptCode{
 				Type: READOUT,
 			})
 			hasOutput = true
 		case ',':
 			if last.Type != -1 {
-				optCodes = append(optCodes, last)
+				optCodes.Push(last)
 			}
-			optCodes = append(optCodes, &OptCode{
+			optCodes.Push(&OptCode{
 				Type: READIN,
 			})
 			hasReadin = true
 		case '[':
 			if last.Type != -1 {
-				optCodes = append(optCodes, last)
+				optCodes.Push(last)
 			}
-			optCodes = append(optCodes, &OptCode{
+			optCodes.Push(&OptCode{
 				Type: BEGINLOOP,
 			})
 		case ']':
 			if last.Type != -1 {
-				optCodes = append(optCodes, last)
+				optCodes.Push(last)
 			}
-			optCodes = append(optCodes, &OptCode{
+			optCodes.Push(&OptCode{
 				Type: ENDLOOP,
 			})
 		default:
-			optCodes = append(optCodes, last)
+			optCodes.Push(last)
 			continue
 		}
 	}
 
+	processed := OptimizeForLoops(optCodes)
+
 	// Print out go code!
-	sb := &bytes.Buffer{}
+	sb := &strings.Builder{}
 	tabs := 0
 	addLine(sb, "package main", tabs)
 	if hasOutput {
@@ -166,20 +164,19 @@ func main() {
 	if hasReadin {
 		addLine(sb, "b := make([]byte, 1)", tabs)
 	}
-
-	for _, optCode := range optCodes {
-		switch optCode.Type {
+	for opt := processed.Pop(); processed.HasNext(); opt = processed.Pop() {
+		switch opt.Type {
 		case PTUP:
-			addLine(sb, fmt.Sprintf("ptr = ptr + %d", optCode.Value), tabs)
+			addLine(sb, fmt.Sprintf("ptr += %d", opt.Value), tabs)
 
 		case PTDOWN:
-			addLine(sb, fmt.Sprintf("ptr = ptr - %d", optCode.Value), tabs)
+			addLine(sb, fmt.Sprintf("ptr -= %d", opt.Value), tabs)
 
 		case VALUEUP:
-			addLine(sb, fmt.Sprintf("buffer[ptr] = buffer[ptr] + byte(%d)", optCode.Value), tabs)
+			addLine(sb, fmt.Sprintf("buffer[ptr] += byte(%d)", opt.Value), tabs)
 
 		case VALUEDOWN:
-			addLine(sb, fmt.Sprintf("buffer[ptr] = buffer[ptr] - byte(%d)", optCode.Value), tabs)
+			addLine(sb, fmt.Sprintf("buffer[ptr] -= byte(%d)", opt.Value), tabs)
 
 		case READOUT:
 			addLine(sb, "fmt.Print(string(buffer[ptr]))", tabs)
@@ -187,6 +184,9 @@ func main() {
 		case READIN:
 			addLine(sb, "os.Stdin.Read(b)", tabs)
 			addLine(sb, "buffer[ptr] = b[0]", tabs)
+
+		case SETZERO:
+			addLine(sb, "buffer[ptr] = 0", tabs)
 
 		case BEGINLOOP:
 			addLine(sb, "for buffer[ptr] != 0 {", tabs)
@@ -200,18 +200,91 @@ func main() {
 	tabs--
 	addLine(sb, "}", tabs)
 
-	err = ioutil.WriteFile(outputFile, sb.Bytes(), 0644)
+	err = ioutil.WriteFile(outputFile, []byte(sb.String()), 0644)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-
 }
 
-func addLine(sb *bytes.Buffer, value string, tabs int) {
+func OptimizeForLoops(optCodes *FIFO) *FIFO {
+	output := NewFifo()
+	for opt := optCodes.Pop(); optCodes.HasNext(); opt = optCodes.Pop() {
+		// This optimization is quite simple.  We are looking for what is essentially [-]
+		// This resets the value at current pointer to 0, so we are looking for this
+		// and just setting the value to 0
+		if opt.Type == BEGINLOOP {
+			opt1 := optCodes.Pop()
+			opt2 := optCodes.Pop()
+			if opt1.Type == VALUEDOWN && opt2.Type == ENDLOOP {
+				output.Push(&OptCode{Type: SETZERO})
+				continue
+			} else {
+				// Not a reset, put the values back onto the queue like it never happened
+				optCodes.PushHead(opt2)
+				optCodes.PushHead(opt1)
+			}
+		}
+		output.Push(opt)
+	}
+	return output
+}
+
+func addLine(sb *strings.Builder, value string, tabs int) {
 	for i := 0; i < tabs; i++ {
 		sb.WriteString("\t")
 	}
 	sb.WriteString(value)
 	sb.WriteString("\n")
+}
 
+type FIFO struct {
+	queue []*OptCode
+}
+
+func NewFifo() *FIFO {
+	return &FIFO{
+		queue: make([]*OptCode, 0),
+	}
+}
+
+func (f *FIFO) Push(v *OptCode) {
+	f.queue = append(f.queue, v)
+}
+
+func (f *FIFO) PushHead(v *OptCode) {
+	f.queue = append([]*OptCode{v}, f.queue...)
+}
+
+func (f *FIFO) Pop() *OptCode {
+	if len(f.queue) < 1 {
+		return nil
+	}
+	v := f.queue[0]
+	f.queue = f.queue[1:]
+	return v
+}
+
+func (f *FIFO) HasNext() bool {
+	return len(f.queue) > 0
+}
+
+func (f *FIFO) Peak() *OptCode {
+	return f.queue[0]
+}
+
+func (f *FIFO) PeakOffset(off int) *OptCode {
+	if off > len(f.queue) {
+		return nil
+	}
+	return f.queue[off]
+}
+
+func (f *FIFO) PeakLast() *OptCode {
+	return f.queue[len(f.queue)-1]
+}
+
+func (f *FIFO) PopLast() *OptCode {
+	v := f.queue[len(f.queue)-1]
+	f.queue = f.queue[:len(f.queue)-1]
+	return v
 }
